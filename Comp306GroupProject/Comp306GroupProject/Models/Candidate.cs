627fa87d7141d557e3fd85bae6817542941de855
Sindhu Binil <70276570+Ann8002@users.noreply.github.com>
2022-12-08 00:07:57 -0500

    Test

---
+++ Comp306GroupProject/Comp306GroupProject/Models/Candidate.cs
@@ -0,0 +1,14 @@
+namespace Comp306GroupProject.Models
+
+{
+    public partial class Candiate
+    {
+        public int CandidateId { get; set; }
+        public string CandidateName { get; set; }
+        public string CandidateEmail { get; set; }
+        public string CandidateMajor { get; set; }
+        public string CandidateSkill { get; set; }
+        public string CandidateCity { get; set; }
+        public string CandidateCountry { get; set; }
+    }
+}
