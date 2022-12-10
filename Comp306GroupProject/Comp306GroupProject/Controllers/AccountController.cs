9f0706c605bea17524f58a27e29e3338bec76ad9
casronjohnson <casronjohnson@gmail.com>
2022-12-10 12:41:27 -0500

    Added AccountController Added Account View Folder Added new Layout Added logo

---
+++ Comp306GroupProject/Comp306GroupProject/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Comp306GroupProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
